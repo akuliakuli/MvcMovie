25ebe9d79baa89f7ae9b37255cd75a8f541179ca
akuliakuli <kabby889@gmail.com>
2023-02-20 23:05:03 +0400

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //
+        // GET /HelloWorld/Welcome
+        public string Welcome(int id)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {id} go to vault fast");
+        }
+
+    }
+}
